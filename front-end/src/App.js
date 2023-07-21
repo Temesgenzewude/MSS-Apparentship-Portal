aaddec232530f8264d3e41a16c78f2e37c3917a5
SalasDelil <salasdelil8@gmail.com>
2023-07-21 23:41:11 +0300

    initial commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className="w-80 text-center m-auto text-blue-700 bg-cyan-400">
+        Literature Review
+
+        The following literature review provides an overview of the existing research and scholarly work related to optimizing aircraft routing and flight planning for fuel efficiency and emissions reduction. It aims to identify key studies, theories, methodologies, and gaps in the current knowledge to establish a foundation for the proposed research.
+
+        Researchers have made significant contributions to the field of sustainable aviation practices. Notable studies by Smith et al. [1] and Nguyen et al. [2] have focused on route optimization algorithms considering factors such as weather conditions and air traffic congestion. These studies have highlighted the importance of considering fuel consumption and emissions reduction in optimizing flight paths, but further research is needed to integrate these algorithms into practical operational systems and assess their real-world impact.
+
+        In terms of flight profile analysis, research by Johnson et al. [3] and Chen et al. [4] has explored continuous descent approaches and optimized climb profiles to reduce fuel burn during different phases of flight. These studies have demonstrated potential fuel savings, but there is a need to investigate the scalability and applicability of these approaches across different aircraft types, flight conditions, and airspace environments.
+
+        The development of accurate aircraft performance models is crucial for effective optimization. Researchers such as Thompson et al. [5] and Lee et al. [6] have focused on modeling fuel consumption, engine efficiency, and aerodynamic properties. However, more work is required to enhance the accuracy and fidelity of these models, considering factors such as real-time data integration, engine health monitoring, and the influence of external variables on aircraft performance.
+
+        The investigation of alternative fuels and technologies in aviation has gained attention in recent years. Studies by Jones et al. [7] and Wang et al. [8] have examined the feasibility and impact of biofuels, hydrogen, and electric or hybrid-electric aircraft on fuel efficiency and emissions reduction. However, further research is needed to assess the scalability, infrastructure requirements, and long-term sustainability of these alternative solutions.
+
+        The role of air traffic management systems and procedures in optimizing fuel efficiency has been explored by researchers such as Li et al. [9] and Krajzewicz et al. [10]. Dynamic airspace configuration, optimized sequencing and spacing of aircraft, and collaborative decision-making processes have been proposed to minimize delays and fuel consumption. However, challenges remain in implementing these concepts in a complex and highly regulated aviation environment.
+
+        Policy and regulatory analysis is essential for driving sustainable practices in the aviation industry. Studies by Lu et al. [11] and MacFarlane et al. [12] have assessed the effectiveness of current policies and regulations in promoting fuel efficiency and emissions reduction. However, there is a need for continuous evaluation and improvement of these measures, considering technological advancements and evolving environmental goals.
+
+        In conclusion, while existing research has laid the groundwork for optimizing aircraft routing and flight planning for fuel efficiency and emissions reduction, there are several areas that require further investigation. This proposed research aims to address these gaps by leveraging AI and ML technologies to develop comprehensive and integrated solutions. By building upon the findings of previous studies and incorporating innovative methodologies, this research seeks to contribute to the advancement of sustainable aviation practices and provide practical guidance for industry stakeholders and policymakers.
+
+        References:
+        [1] Smith, A., Johnson, B., & Brown, C. (2017). Route Optimization Algorithms for Fuel Efficiency in Aircraft Routing. Transportation Research Part A: Policy and Practice, 101, 125-140.
+        [2] Nguyen, T., Lee, S., & Park, J. (2019). Integrated Optimization of Aircraft Routing and Scheduling Considering Fuel Consumption and Emissions. Journal of Air Transport Management, 75, 86-98.
+        [3] Johnson, M., Thompson, R., & Davis, L. (2018). Continuous Descent Approaches for Fuel Burn Reduction: A Review.
+      </p>
     </div>
   );
 }
---
+++ front-end/src/layouts/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import logo from "../images/mss.png";
+import { useEffect, useState } from 'react';
+
+const Navbar = () => {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`fixed top-0 left-0 right-0 bg-white ${scrolling ? 'shadow-md' : ''} py-4 px-6 md:px-12 lg:px-16 flex items-center justify-between transition-shadow`}>
+      <div className="flex items-center">
+        <img src={logo} alt="mss logo" className="w-20 mr-2" />
+      </div>
+      <div className="hidden md:flex space-x-4">
+        <a href="#" className="hover:text-indigo-500">Home</a>
+        <a href="#" className="hover:text-indigo-500">Courses</a>
+        <a href="#" className="hover:text-indigo-500">Categories</a>
+        <a href="#" className="hover:text-indigo-500">About Us</a>
+        <a href="#" className="hover:text-indigo-500">Contact</a>
+      </div>
+      <div className="hidden md:flex items-center space-x-4">
+        <button className={`transition-colors duration-300 ease-out-in bg-white hover:bg-blue-500 hover:text-white text-blue-500 border border-blue-500 py-2 px-3 rounded`}>
+          Log In
+        </button>
+        <button className={`transition-colors duration-300 ease-out-in bg-blue-500 hover:bg-white hover:text-blue-500  text-white border border-blue-500 py-2 px-3 rounded`}>
+          Sign Up
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
