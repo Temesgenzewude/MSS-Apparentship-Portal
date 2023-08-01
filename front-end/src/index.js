f70c3769415248a6832b310b4175091da396f689
SalasDelil <salasdelil8@gmail.com>
2023-08-01 16:43:21 +0300

    Updated

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './layouts/Navbar';
-import HomeLandingPage from './components/HomeLandingPage';
-import Layout from './layouts/Layout';
+import HomePage from './components/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Layout>
-      <Navbar />
-      <HomeLandingPage />
-    </Layout>
+    <HomePage />
     <App />
   </React.StrictMode>
 );
