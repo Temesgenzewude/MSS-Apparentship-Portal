d438b252e21727d748e740016b2a15fcc3193be8
SalasDelil <salasdelil8@gmail.com>
2023-09-02 17:24:53 +0300

    signup and homepage update

--- front-end/src/components/HomePage.js
+++ front-end/src/components/HomePage.js
@@ -69,39 +69,39 @@ function HomePage() {
             <Layout>
                 <Navbar />
                 <HomeLandingPage />
-            </Layout>
-            <section id='courses' className="md:px-12 lg:px-16 mx-auto p-4">
-                <div className='mb-6 flex items-center flex-col'>
-                    <h1 className="text-3xl font-semibold text-customBlue mb-4 text-center">
-                        Welcome to Our Course Recommendations
-                    </h1>
-                    <p className="text-customBlue mb-8 text-center">
-                        Check out our top-rated courses to enhance your skills!
-                    </p>
-                    <div className="w-full flex justify-end">
-                        <button className='rounded-md shadow-lg transition-shadow duration-500 hover:shadow-xl p-2 mr-4 mb-4 text-white bg-customPurple'>
-                            <a href='/courses'>View more courses</a>
-                        </button>
-                    </div>
-                </div>
-                <div className="flex flex-wrap justify-center">
-                    {courses.map((course) => (
-                        <div key={course.id} className="w-1/2 lg:w-1/4 px-2 mb-8">
-                            <a to={`/courses/${course.id}`}>
-                                <CourseCard course={course} />
-                            </a>
+                <section id='courses' className="md:px-12 lg:px-16 mx-auto p-4">
+                    <div className='mb-6 flex items-center flex-col'>
+                        <h1 className="text-3xl font-semibold text-customBlue mb-4 text-center">
+                            Welcome to Our Course Recommendations
+                        </h1>
+                        <p className="text-customBlue mb-8 text-center">
+                            Check out our top-rated courses to enhance your skills!
+                        </p>
+                        <div className="w-full flex justify-end">
+                            <button className='rounded-md shadow-lg transition-shadow duration-500 hover:shadow-xl p-2 mr-4 mb-4 text-white bg-customPurple'>
+                                <a href='/courses'>View more courses</a>
+                            </button>
                         </div>
-                    ))}
+                    </div>
+                    <div className="flex flex-wrap justify-center">
+                        {courses.map((course) => (
+                            <div key={course.id} className="w-1/2 lg:w-1/4 px-2 mb-8">
+                                <a to={`/courses/${course.id}`}>
+                                    <CourseCard course={course} />
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+                <div id='internship' className="flex justify-center items-center md:px-24 lg:px-32 mx-auto pt-24 px-8">
+                    <InternshipCard
+                        title="Software Developer Internship"
+                        description="Work on exciting projects and gain valuable experience in software development."
+                        available={true}
+                    />
                 </div>
-            </section>
-            <div id='internship' className="flex justify-center items-center md:px-24 lg:px-32 mx-auto pt-24 px-8">
-                <InternshipCard
-                    title="Software Developer Internship"
-                    description="Work on exciting projects and gain valuable experience in software development."
-                    available={true}
-                />
-            </div>
-            <TestimonialSlider />
+                <TestimonialSlider />
+            </Layout>
             <Footer />
         </div>
     )
